f2e1dcb433bd4d22ab4628c2698d95ba84a11e5f
konabe <rt.konabe@gmail.com>
2025-10-18 00:01:35 +0000

    chore(logger): remove NullLogger and simplify create_logger

--- src/infrastructure/logger.py
+++ src/infrastructure/logger.py
@@ -30,32 +30,5 @@ class StdLogger:
         self._logger.exception(msg)
 
 
-class NullLogger:
-    def debug(self, msg: str) -> None:
-        # テストや一時的にログ出力を無効化したい場合のダミー実装です。意図的に何もしません。
-        _ = ("debug", msg)
-        return None
-
-    def info(self, msg: str) -> None:
-        # テストや一時的にログ出力を無効化したい場合のダミー実装です。意図的に何もしません。
-        _ = ("info", msg)
-        return None
-
-    def warning(self, msg: str) -> None:
-        # テストや一時的にログ出力を無効化したい場合のダミー実装です。意図的に何もしません。
-        _ = ("warning", msg)
-        return None
-
-    def error(self, msg: str) -> None:
-        # テストや一時的にログ出力を無効化したい場合のダミー実装です。意図的に何もしません。
-        _ = ("error", msg)
-        return None
-
-    def exception(self, msg: str) -> None:
-        # テストや一時的にログ出力を無効化したい場合のダミー実装です。意図的に何もしません。
-        _ = ("exception", msg)
-        return None
-
-
-def create_logger(name: str = __name__, use_null: bool = False) -> Logger:
-    return NullLogger() if use_null else StdLogger(name)
+def create_logger(name: str = __name__) -> Logger:
+    return StdLogger(name)
