4350914b1201595f677461b6ff2be97b7e1c1d89
konabe <rt.konabe@gmail.com>
2025-10-26 16:52:08 +0000

    fix(line_model): export create_pokemon_zukan_button_template

--- src/infrastructure/line_model/__init__.py
+++ src/infrastructure/line_model/__init__.py
@@ -1,11 +1,5 @@
-"""LINE related model helpers (message builders, etc.)."""
-
-from .zukan_flex import (
-	create_pokemon_zukan_button_template,
-	create_pokemon_zukan_flex_dict,
-)
+from .zukan_flex import create_pokemon_zukan_button_template
 
 __all__ = [
 	"create_pokemon_zukan_button_template",
-	"create_pokemon_zukan_flex_dict",
 ]
